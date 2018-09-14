8b369691276c74ef9a3b78248fc8b91358000242
Coding Skills <test.codingskill@gmail.com>
2018-09-14 23:13:42 +1000

    first commit

---
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,50 @@
+import { Params, ActivatedRoute } from "@angular/router";
+import { RecipeService } from "./../../services/recipe.service";
+import { ShoppinglistService } from "./../../services/shoppinglist.service";
+import { Recipe } from "./../recipe.model";
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-recipe-detail",
+  templateUrl: "./recipe-detail.component.html",
+  styleUrls: ["./recipe-detail.component.css"]
+})
+
+export class RecipeDetailComponent implements OnInit {
+
+  recipe: Recipe;
+  id: number;
+  length = this.recipeService.getRecipes().length;
+
+  constructor(public shoppingListService: ShoppinglistService, public route: ActivatedRoute, public recipeService: RecipeService) { }
+
+  ngOnInit() {
+    const id = this.route.params.subscribe((params: Params) => {
+    this.id = +params["id"];
+    this.recipe = this.recipeService.getRecipe(this.id);
+    });
+
+    // listen and subscribe to recipe changes and update through observable
+    this.recipeService.recipesChanged.subscribe(
+      (recipe: Recipe[]) => {
+        this.recipe = recipe[this.id];
+      });
+  }
+
+  // add ingredients to shopping list
+  addToShoppingList() {
+    this.recipe.ingredients.forEach(ingredient => {
+      this.shoppingListService.addIngredients(ingredient);
+    });
+  }
+
+  // delete a recipe
+  onDeleteRecipe() {
+    this.recipeService.removeRecipe(this.id);
+  }
+
+}
+
+
+
+
---
+++ src/app/services/shoppinglist.service.ts
@@ -0,0 +1,35 @@
+import { Ingredient } from "./../shared/ingredient.model";
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs/Subject";
+
+@Injectable()
+export class ShoppinglistService {
+  amountInput: any;
+  nameInput: any;
+  startedEditing = new Subject<number>();
+
+
+  public ingredients: Ingredient[] = [
+    new Ingredient("Apple", 10),
+    new Ingredient("Tomatoes", 10),
+    new Ingredient("Pears", 10)
+  ];
+
+
+  getIngredient(index: number): Ingredient {
+    return this.ingredients[index];
+  }
+
+  addIngredients(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
+
+  editIngredient(index: number, item: Ingredient) {
+    this.ingredients[index] = item;
+  }
+
+  deleteIngredient(index: number) {
+    this.ingredients.splice(index, 1);
+  }
+
+}
